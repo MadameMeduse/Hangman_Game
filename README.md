a3f6ed6f4a5109957f364427e170edf82ed96acb
m1waligora@gmail.com <53176738+Sonfinity-Poland@users.noreply.github.com>
2019-10-25 14:56:16 +0200

    update changes

---
+++ README.md
@@ -0,0 +1,74 @@
+# Hangman_Game
+
+http://hangman.pss.ogicom.pl/
+
+<h1>Architektura aplikacji:</h1>
+<ol>
+<li>
+  <p>Najpierw ustaliłam stan początkowy czyli initialState w którym zawarte jest:</p>
+</li>
+    <ul>
+        <li>lives: ilość żyć, liczba </li>
+        <li>used: used z tablicy letters [ ]</li>
+        <li>word: słowo pobrane jako string z API</li>
+    </ul>
+<li>
+   <p>Następnie ustaliłam co ma być zawarte w widoku aplikacji: -- > element (zależny od):</p>
+  <br>
+  </li>
+    <ul>
+        <li>alfabet (used) - widok zmienia się w zależności od wykorzystanych już liter</li>
+        <li>liczba żyć (lives) - zmienia się w zależności od ilości pozostałych żyć </li>
+        <li>słowo (word, used) - zmienia się z zależności od słowa i wykorzystanych liter</li>
+        <li>hangman (lives) -  zmienia się w zależności od liczby żyć </li>
+        <li>loading (word) - zmienia się w zależności od słowa. Jeśli słowa nie ma, pokazuje się ładowanie.</li>
+        <li>final screen (lives, word, used) - ekran końcowy w zależności od ilości żyć, słowa i wykorzystanych liter.</li>
+    </ul>
+<li>
+   <p>Ustaliłam eventy jakie mają wystąpić w aplikacji:</p>
+</li>
+<li>
+ <ul>
+  <p>
+    <b>click</b> 
+    po kliknięciu w literkę</p>
+  </li>
+      <ul>
+ <li>dodajemy do used</li>
+  <li>jeśli nie pasuje do słowa =></li>
+   <li>lives-- (odejmujemy życie)</li>
+    </ul>
+</ul>
+<li>
+   <p>Final screen:</p></li>
+
+<ul> Wygrana jeśli:
+    <li>lives > 0</li>
+    <li>wszystkie literki z word są w used (nie licząc pierwszej i ostatniej)</li>
+</ul>
+
+<li> 
+  <p>Słabe strony aplikacji:</p>
+</li>
+ <ul>
+    <li>Brak webpacka - wszystkie funkcje w jednym pliku js  </li>
+    <li>Wrzucenie projektu na github na sam koniec zamiast na początku (to było bardzo nierozsądne) </li>
+    <li>Gra źle wyświetla się na poziomym widoku komórki komórki </li>
+    <li>Canva hangman wygląda brzydko, gdybym miała więcej czasu wyglądałaby lepiej</li>
+    <li>API nie posiada certyfikatu stąd nie da się odpalić strony na github pages i byłam zmuszona wrzucić na hosting</li>
+ </ul>
+
+ <li> 
+  <p>Aby uruchomić grę należy kliknąc w link na samej górze lub:</p>
+</li>
+ <ul>
+    <li>Uruchomic za pomocą liveserver 
+        <br> <b>npm install -g live-server</b> 
+        <br>a następnie 
+        <br> <b>npx live-server </b> 
+    </li>
+    <li>zainstalować SCSS przez npm install -g sass</li>
+    <li>Kompilacja do folderu css za pomocą npx sass --watch scss:css </li>
+    
+ </ul>
+</ol>
--- script.js
+++ script.js
@@ -1,195 +1,213 @@
 (() => {
-    const alphabetElement = document.getElementsByClassName('js-alphabet')[0];
-    const wordElement = document.getElementsByClassName('js-word')[0];
-    const loadingElement = document.getElementsByClassName('js-loading')[0];
-    const livesElement = document.getElementsByClassName('js-lives')[0];
-    const finalScreen = document.getElementsByClassName('js-final-screen')[0];
-    const finalScreenHeader = finalScreen.getElementsByClassName('js-final-screen__header')[0];
-    const finalScreenButton = finalScreen.getElementsByClassName('js-final-screen__button')[0];
-    const canvasElement = document.getElementsByClassName('js-canvas')[0];
+  const alphabetElement = document.getElementsByClassName("js-alphabet")[0];
+  const wordElement = document.getElementsByClassName("js-word")[0];
+  const loadingElement = document.getElementsByClassName("js-loading")[0];
+  const livesElement = document.getElementsByClassName("js-lives")[0];
+  const finalScreen = document.getElementsByClassName("js-final-screen")[0];
+  const finalScreenHeader = finalScreen.getElementsByClassName(
+    "js-final-screen__header"
+  )[0];
+  const finalScreenButton = finalScreen.getElementsByClassName(
+    "js-final-screen__button"
+  )[0];
+  const canvasElement = document.getElementsByClassName("js-canvas")[0];
 
-    const allLetters = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase().split('');
+  const allLetters = "abcdefghijklmnopqrstuvwxyz".toUpperCase().split("");
 
-    const initialState = {
-        lives: 9,
-        word: undefined,
-        used: [],
-    };
+  const initialState = {
+    lives: 9,
+    word: undefined,
+    used: []
+  };
 
-    let state;
+  let state;
 
-    const renderView = () => {
-        const renderAlphabet = () => {
-            alphabetElement.innerHTML = allLetters.map((letter) => (
-                `<button class="js-alphabet-letter alphabet__letter"${state.used.includes(letter) ? ' disabled' : ''}>
+  const renderView = () => {
+    const renderAlphabet = () => {
+      alphabetElement.innerHTML = allLetters
+        .map(
+          letter =>
+            `<button class="js-alphabet-letter alphabet__letter"${
+              state.used.includes(letter) ? " disabled" : ""
+            }>
                     ${letter}
                 </button>`
-            )).join('');
-        };
-
-        const renderWord = () => {
-            if (state.word === undefined) {
-                return '';
-            }
-
-            wordElement.innerHTML = state.word.split("").map((letter, index, allLetters) => {
-                if (index === 0 || index === allLetters.length - 1) {
-                    return `<div class="word__letter">${letter}</div>`;
-                }
-
-                if (state.used.includes(letter)) {
-                    return `<div class="word__letter">${letter}</div>`;
-                }
-
-                return `<div class="word__letter">_</div>`;
-            }).join('');
-        };
-
-        const renderLoading = () => {
-            const hiddenClassName = 'loading--hidden';
-
-            if (state.word === undefined) {
-                loadingElement.classList.remove(hiddenClassName);
-            } else {
-                loadingElement.classList.add(hiddenClassName);
-            }
-        }
-
-        const renderLives = () => {
-            livesElement.innerHTML = `You have <span class="lives__number">${state.lives}</span> lives left`;
-        }
-
-        const renderFinalScreen = () => {
-            const hiddenClassName = 'final-screen--hidden';
-
-            if (state.lives === 0) {
-                finalScreen.classList.remove(hiddenClassName)
-                finalScreenHeader.innerText = 'GAME OVER!';
-                return;
-            }
-
-            if (state.word === undefined) {
-                finalScreen.classList.add(hiddenClassName);
-                return;
-            }
-
-            const wordWithoutFirstAndLastLetter = state.word.slice(1, -1);
-
-            if (wordWithoutFirstAndLastLetter.split('').every((letter) => state.used.includes(letter))) {
-                finalScreen.classList.remove(hiddenClassName)
-                finalScreenHeader.innerText = 'YOU WON THE GAME!';
-                return;
-            }
-
-            finalScreen.classList.add(hiddenClassName);
-        };
-
-        const renderHangman = () => {
-            const canvasContext = canvasElement.getContext('2d');
-
-            const drawLine = (fromX, fromY, toX, toY) => {
-                canvasContext.moveTo(fromX, fromY);
-                canvasContext.lineTo(toX, toY);
-                canvasContext.stroke();
-            }
-
-            canvasContext.beginPath();
-            canvasContext.strokeStyle = "black";
-            canvasContext.lineWidth = 30;
-
-            canvasContext.clearRect(0, 0, canvasElement.width, canvasElement.height);
-
-            drawLine(0, 1500, 1500, 1500);
-
-            if (state.lives < 9) {
-                drawLine(100, 0, 100, 6000);
-            }
-
-            if (state.lives < 8) {
-                drawLine(0, 50, 700, 50);
-            }
-
-            if (state.lives < 7) {
-                drawLine(600, 50, 600, 150);
-            }
-
-            if (state.lives < 6) {
-                canvasContext.beginPath();
-                canvasContext.arc(600, 250, 100, 0, Math.PI * 2, true);
-                canvasContext.stroke();
-            }
-
-            if (state.lives < 5) {
-                drawLine(600, 360, 600, 700)
-            }
-
-            if (state.lives < 4) {
-                drawLine(600, 460, 200, 500);
-            }
-
-            if (state.lives < 3) {
-                drawLine(600, 460, 1000, 500);
-            }
-
-            if (state.lives < 2) {
-                drawLine(600, 700, 200, 1000);
-            }
-
-            if (state.lives < 1) {
-                drawLine(600, 700, 1000, 1000);
-            }
-        };
-
-        renderLives();
-        renderAlphabet();
-        renderWord();
-        renderLoading();
-        renderFinalScreen();
-        renderHangman();
+        )
+        .join("");
     };
 
-    const fetchNewWord = async () => {
-        try {
-            const response = await fetch("http://hangman.career.wecode.stage.wecode.agency/api/v1/word/random");
-            const json = await response.json();
+    const renderWord = () => {
+      if (state.word === undefined) {
+        return "";
+      }
 
-            return json.word;
-        } catch {
-            alert('Something went wrong!');
-        }
+      wordElement.innerHTML = state.word
+        .split("")
+        .map((letter, index, allLetters) => {
+          if (index === 0 || index === allLetters.length - 1) {
+            return `<div class="word__letter">${letter}</div>`;
+          }
+
+          if (state.used.includes(letter)) {
+            return `<div class="word__letter">${letter}</div>`;
+          }
+
+          return `<div class="word__letter">_</div>`;
+        })
+        .join("");
     };
 
-    const newGame = async () => {
-        state = { ...initialState };
+    const renderLoading = () => {
+      const hiddenClassName = "loading--hidden";
 
-        renderView();
-
-        const newWord = await fetchNewWord();
-
-        state.word = newWord.toUpperCase();
-        renderView();
+      if (state.word === undefined) {
+        loadingElement.classList.remove(hiddenClassName);
+      } else {
+        loadingElement.classList.add(hiddenClassName);
+      }
     };
 
-    const guess = (letter) => {
-        state.used = [...state.used, letter];
-        const wordWithoutFirstAndLastLetter = state.word.slice(1, -1);
-
-        if (!wordWithoutFirstAndLastLetter.includes(letter)) {
-            state.lives--;
-        }
-
-        renderView();
+    const renderLives = () => {
+      livesElement.innerHTML = `You have <span class="lives__number">${state.lives}</span> lives left`;
     };
 
-    alphabetElement.addEventListener('click', (event) => {
-        if (!event.target.classList.contains('js-alphabet-letter')) {
-            return;
-        }
+    const renderFinalScreen = () => {
+      const hiddenClassName = "final-screen--hidden";
 
-        guess(event.target.innerText);
-    });
+      if (state.lives === 0) {
+        finalScreen.classList.remove(hiddenClassName);
+        finalScreenHeader.innerText = "GAME OVER!";
+        return;
+      }
 
-    finalScreenButton.addEventListener('click', newGame);
+      if (state.word === undefined) {
+        finalScreen.classList.add(hiddenClassName);
+        return;
+      }
 
-    newGame();
+      const wordWithoutFirstAndLastLetter = state.word.slice(1, -1);
+
+      if (
+        wordWithoutFirstAndLastLetter
+          .split("")
+          .every(letter => state.used.includes(letter))
+      ) {
+        finalScreen.classList.remove(hiddenClassName);
+        finalScreenHeader.innerText = "YOU WON THE GAME!";
+        return;
+      }
+
+      finalScreen.classList.add(hiddenClassName);
+    };
+
+    const renderHangman = () => {
+      const canvasContext = canvasElement.getContext("2d");
+
+      const drawLine = (fromX, fromY, toX, toY) => {
+        canvasContext.moveTo(fromX, fromY);
+        canvasContext.lineTo(toX, toY);
+        canvasContext.stroke();
+      };
+
+      canvasContext.beginPath();
+      canvasContext.strokeStyle = "black";
+      canvasContext.lineWidth = 30;
+
+      canvasContext.clearRect(0, 0, canvasElement.width, canvasElement.height);
+
+      drawLine(0, 1500, 1500, 1500);
+
+      if (state.lives < 9) {
+        drawLine(100, 0, 100, 6000);
+      }
+
+      if (state.lives < 8) {
+        drawLine(0, 50, 700, 50);
+      }
+
+      if (state.lives < 7) {
+        drawLine(600, 50, 600, 150);
+      }
+
+      if (state.lives < 6) {
+        canvasContext.beginPath();
+        canvasContext.arc(600, 250, 100, 0, Math.PI * 2, true);
+        canvasContext.stroke();
+      }
+
+      if (state.lives < 5) {
+        drawLine(600, 360, 600, 700);
+      }
+
+      if (state.lives < 4) {
+        drawLine(600, 460, 200, 500);
+      }
+
+      if (state.lives < 3) {
+        drawLine(600, 460, 1000, 500);
+      }
+
+      if (state.lives < 2) {
+        drawLine(600, 700, 200, 1000);
+      }
+
+      if (state.lives < 1) {
+        drawLine(600, 700, 1000, 1000);
+      }
+    };
+
+    renderLives();
+    renderAlphabet();
+    renderWord();
+    renderLoading();
+    renderFinalScreen();
+    renderHangman();
+  };
+
+  const fetchNewWord = async () => {
+    try {
+      const response = await fetch(
+        "http://hangman.career.wecode.stage.wecode.agency/api/v1/word/random"
+      );
+      const json = await response.json();
+
+      return json.word;
+    } catch {
+      alert("Something went wrong!");
+    }
+  };
+
+  const newGame = async () => {
+    state = { ...initialState };
+
+    renderView();
+
+    const newWord = await fetchNewWord();
+
+    state.word = newWord.toUpperCase();
+    renderView();
+  };
+
+  const guess = letter => {
+    state.used = [...state.used, letter];
+    const wordWithoutFirstAndLastLetter = state.word.slice(1, -1);
+
+    if (!wordWithoutFirstAndLastLetter.includes(letter)) {
+      state.lives--;
+    }
+
+    renderView();
+  };
+
+  alphabetElement.addEventListener("click", event => {
+    if (!event.target.classList.contains("js-alphabet-letter")) {
+      return;
+    }
+
+    guess(event.target.innerText);
+  });
+
+  finalScreenButton.addEventListener("click", newGame);
+
+  newGame();
 })();
